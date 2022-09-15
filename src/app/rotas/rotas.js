7d4ebee82f75a7174c4d7dd3e153e37bad093508
Felipe Jorge <felipejorgecd@gmail.com>
2022-09-15 13:37:10 -0300

    commit db create

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -32,6 +32,26 @@ module.exports = (app) => {
                 .catch(erro => console.log(erro));
     
     });
+
+
+    app.get('/livros/form' , function (req, resp){
+        resp.marko(require('../views/livros/form/form.marko'));
+    } )
+
+    app.post('/livros', function(req,resp){
+      console.log(req.body);
+      const livroDao = new LivroDao(db);
+        livroDao.adiciona(req.body)
+                .then(livros => resp.marko(
+                    require('../views/livros/lista/lista.marko'),
+                    {
+                        livros: livros
+                    }
+    
+                ))
+                .catch(erro => console.log(erro));
+    
+    })
     }
   
 
